f701e443058e7fc04240e407057e3fdecdcaa635
RamaMaulana22 <80238431+RamaMaulana22@users.noreply.github.com>
2021-03-20 18:41:51 +0700

    Add files via upload

---
+++ Pertemuan4/Perkalian.php
@@ -0,0 +1,15 @@
+<?php
+$a = 1;
+while($a <= 10){
+    $b = 1;
+    while($b <=10){
+        $kali =$a * $b;
+        echo"<table>";
+        echo"<table>$a</tr> <tr> x </tr> <tr>$b</tr> <tr> = </tr> <tr>$kali</tr></td>";
+        echo"</table>";
+        $b++;
+    }
+    $a++;
+}
+
+?>
